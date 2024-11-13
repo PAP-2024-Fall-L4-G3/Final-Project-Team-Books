f29b7538ae1beab250f66b3cc20befe09d290ef5
Rongrui Zhu <ronrzhu@gmail.com>
2024-11-12 17:23:00 -0800

    Subject buttons

--- bookAPI.js
+++ bookAPI.js
@@ -5,12 +5,22 @@ const API_KEY = 'AIzaSyDQ58dHc4Xza5p60pM_CMbzHy0MKYu2y1o';
 book.volumeInfo.imageLinks.thumbnail; <- IMAGE SRC
 book.volumeInfo.description; */
 
-export async function fetchBooks(input) {
-    const url = `${BOOK_URL}intitle:${input}&key=${API_KEY}`;
+async function fetchAPI(query) {
     try {
-        const response = await fetch(url);
+        console.log(query);
+        const response = await fetch(`${BOOK_URL}${query}`);
         return await response.json();
     } catch (error) {
         console.error('Error while fetching API data: ', error);
     }
+}
+
+export async function fetchBooks(input) {
+    const query = `intitle:${input}&maxResults=36&key=${API_KEY}`;
+    return fetchAPI(query);
+}
+
+export async function fetchBooksBySubject(subject) {
+    const query = `subject:${subject}&maxResults=36&key=${API_KEY}`;
+    return fetchAPI(query);
 }
